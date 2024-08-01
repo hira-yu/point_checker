051c7925f474d563c748db5b3a5b2bf42504830a
Choco2807 <82220979+Choco2807@users.noreply.github.com>
2024-08-02 07:03:27 +0900

    commit v1.0

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$("#check").on('click', function(){
+    let test = (Number($("#test_1").val()) + Number($("#test_2").val())) * 0.1;
+
+    let rep = (
+        Number($("#rep_1").val()) +
+        Number($("#rep_2").val()) +
+        Number($("#rep_3").val()) +
+        Number($("#rep_4").val()) +
+        Number($("#rep_5").val()) +
+        Number($("#rep_6").val()) +
+        Number($("#rep_7").val()) +
+        Number($("#rep_8").val()) +
+        Number($("#rep_9").val()) +
+        Number($("#rep_10").val()) +
+        Number($("#rep_11").val()) +
+        Number($("#rep_12").val()) +
+        Number($("#rep_13").val()) +
+        Number($("#rep_14").val()) +
+        Number($("#rep_15").val())
+    ) / 37.5;
+
+    console.log(rep);
+    $("#result").text("科目評価100のうち，小テストで" + test + "点，レポートで" + rep + "点あるので，" + (60 - test - rep) + "点以上とればおｋ");
+})
